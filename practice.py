9a4033d247a226d0368aaa9c6f3e4cfa8e964fd4
Priscillah <nalubegapriscillah@gmail.com>
2024-01-10 21:58:52 +0300

    more practice

--- practice.py
+++ practice.py
@@ -8,3 +8,15 @@ print ("Hello world"*4)
 1 + 2
 #If it doesn’t print anything, what changes can you make to the program to print the value?
 print( 1+2)
+
+# What will be output of the following program?
+x = 4
+y = x +1
+x = 2
+print (x,y)
+
+#what will be the output of the following program
+x, y = 2, 6
+x, y = y, x+2
+print (x,y)
+
