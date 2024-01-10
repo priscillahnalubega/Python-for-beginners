0d5ca1c78a942a6ebfa853e6f94e97e6ba7a448f
Priscillah <nalubegapriscillah@gmail.com>
2024-01-10 21:34:02 +0300

    more practice

--- practice.py
+++ practice.py
@@ -3,3 +3,8 @@ print (2+3)
 
 #problem2: print Hello world!  four times 
 print ("Hello world"*4)
+
+#Create a python script with the following text and see the output
+1 + 2
+#If it doesn’t print anything, what changes can you make to the program to print the value?
+print( 1+2)
