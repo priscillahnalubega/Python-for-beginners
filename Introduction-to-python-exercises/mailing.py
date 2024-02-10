70d540168dc87aa3ba15ed31180ee5bf56b90cab
Priscillah <nalubegapriscillah@gmail.com>
2024-02-10 09:55:29 +0300

    first exercises

---
+++ Introduction-to-python-exercises/mailing.py
@@ -0,0 +1,8 @@
+"""
+Create a program that displays your name and complete email address formated in a manner that you would usually see it on the the outside of an envelop. Your program
+does not need to read any input from the user.
+"""
+Name = "Mary Doe"
+Email_address = "marydoe@example.com"
+
+print(f"    {Name} \n    {Email_address}")
