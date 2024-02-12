ca6813f057c47eca52c27f32a0bf62797d156c73
Priscillah <nalubegapriscillah@gmail.com>
2024-02-12 09:28:31 +0300

    new update

---
+++ Introduction-to-python-exercises/11.fuel-efficiency.py
@@ -0,0 +1,14 @@
+"""
+In the United States, fuel efficiency for vehicles is normally expressed in miles-pergallon (MPG). In Canada, fuel efficiency is normally expressed in liters-per-hundred
+kilometers (L/100 km). Use your research skills to determine how to convert from
+MPG to L/100 km. Then create a program that reads a value from the user in American
+units and displays the equivalent fuel efficiency in Canadian units
+"""
+def MPG_to_l_per_100km (MPG):
+    return 235.214583 / MPG
+
+MPG = float(input("Enter fuel efficiecy in MPG"))
+
+l_per_100km = MPG_to_l_per_100km
+
+print(f"The equivalent fuel efficiency in l_per_100km is {l_per_100km:.2f} 1/100km")
