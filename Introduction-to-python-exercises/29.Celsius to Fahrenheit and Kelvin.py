a9896b35e628fdbba0594c89c370e6085bc2ecba
Priscillah <nalubegapriscillah@gmail.com>
2024-03-13 13:31:06 +0300

    added a new exercise; celsisus to fahrenheit

--- Introduction-to-python-exercises/29.Celsius to Fahrenheit and Kelvin.py
+++ Introduction-to-python-exercises/29.Celsius to Fahrenheit and Kelvin.py
@@ -6,3 +6,10 @@ units of temperature can be found on the internet.
 """
 #Ask for user input
 Temperature_in_celsius = float(input("Enter temperature in degrees celsius: "))
+
+#Converting Temperature from degrees celcius to degrees fahrenheit
+Temperature_in_Fahrenheit = (9/5 *Temperature_in_celsius ) + 32
+Temperature_in_Kelvin = 273.15 + Temperature_in_celsius
+
+#Diplaying in degree Fahrenheit and Kelvins
+print(f"The temperature in Fahrenheit is {Temperature_in_Fahrenheit: .2F}˚F and in Kelvin is {Temperature_in_Kelvin: .2F}K")
