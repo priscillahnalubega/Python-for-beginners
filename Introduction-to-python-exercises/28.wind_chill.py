23840cd30920110701fbb3781257ce6b708d056d
Priscillah <nalubegapriscillah@gmail.com>
2024-03-13 13:13:17 +0300

    new exercise on wind chill index

--- Introduction-to-python-exercises/28.wind_chill.py
+++ Introduction-to-python-exercises/28.wind_chill.py
@@ -6,7 +6,7 @@ In 2001, Canada, the United Kingdom and the United States adopted the following 
 air temperature in degrees Celsius and V is the wind speed in kilometers per hour.
 A similar formula with different constant values can be used with temperatures in
 degrees Fahrenheit and wind speeds in miles per hour.
-WCI = 13.12 + 0.6215Ta - 11.37V 0.16 + 0.3965TaV 0.16
+WCI = 13.12 + 0.6215Ta - 11.37V**0.16 + 0.3965Tav**0.16
 Write a program that begins by reading the air temperature and wind speed from the
 user. Once these values have been read your program should display the wind chill
 index rounded to the closest integer.
@@ -14,3 +14,16 @@ The wind chill index is only considered valid for temperatures less than or
 equal to 10 degrees Celsius and wind speeds exceeding 4.8 kilometers per
 hour.
 """
+
+# Get user input
+V = float(input("Enter the speed of the wind in Km/hr: "))
+Ta = float(input("Enter the temperature of air in Degree Celcius: "))
+
+# Check for validity of the conditions
+if Ta <= 10 and V > 4.8:
+    # Calculating the wind Chill index correctly
+    WCI = 13.12 + (0.6215 * Ta) - 11.37 * (V ** 0.16) + 0.3965 * Ta * (V ** 0.16)
+    # Displaying the Wind Chill Index to the closest integer
+    print(f"The wind chill index is: {round(WCI)}")
+else:
+    print("The wind chill index is not valid for the entered temperature and wind speed.")
