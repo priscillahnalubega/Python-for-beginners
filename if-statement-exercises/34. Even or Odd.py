f4d418f7816722b08978bc0c3a7c3050192339e2
Priscillah <nalubegapriscillah@gmail.com>
2024-03-16 11:49:21 +0300

    new exercise

--- if-statement-exercises/34. Even or Odd.py
+++ if-statement-exercises/34. Even or Odd.py
@@ -2,3 +2,11 @@
 Write a program that reads an integer from the user. Then your program should
 display a message indicating whether the integer is even or odd
 """
+#Ask for user input
+user_integer = int(input("Enter any integer: "))
+
+# validation
+if  user_integer %2  == 0:
+    print(f"The integer {user_integer} is even")
+else:
+    print(f"The integer {user_integer} is odd ")
